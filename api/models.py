f0ccd15a1220473fb77e32a556569189444ba704
Fernando Cedeno <fernandostarlin1012@gmail.com>
2022-08-09 15:25:09 -0400

    The models Movie, Genre and Cast were created

--- api/models.py
+++ api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField(max_length=155)
+
+
+class Cast(models.Model):
+    name = models.CharField(max_length=155)
+
+
+# class Tag(models.Model):
+#     name = models.CharField(max_length=155)
+
+
+class Movie(models.Model):
+    title = models.CharField(max_length=200)
+    synopsis = models.TextField(max_length=1000)
+    genre = models.ForeignKey(Genre, on_delete=models.DO_NOTHING)
+    cast = models.ForeignKey(Cast, on_delete=models.DO_NOTHING)
